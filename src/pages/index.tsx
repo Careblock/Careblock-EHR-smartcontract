04b20a65ffec06ea79f30d7c185251e773fcbc75
TranHuyHiep <tranhuyhiep0710@gmail.com>
2024-12-20 23:12:14 +0700

    Milestone 2: Prototype Development and Testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,46 +2,48 @@ import { useState } from "react";
 import type { NextPage } from "next";
 import { useWallet } from '@meshsdk/react';
 import { CardanoWallet } from '@meshsdk/react';
-import { AssetMetadata, ForgeScript, Mint, NativeScript, resolveNativeScriptHash, Transaction } from "@meshsdk/core";
+import { AssetMetadata, ForgeScript, Mint, Transaction } from "@meshsdk/core";
 
 const Home: NextPage = () => {
   const { connected, wallet } = useWallet();
   const [loading, setLoading] = useState<boolean>(false);
+  const [metadata, setMetadata] = useState<Partial<AssetMetadata>>({});
+  const [recipient, setRecipient] = useState<string>("");
 
   async function createEHR() {
     if (wallet) {
       setLoading(true);
-      // use browser wallet to get address
-      const usedAddress = await wallet.getUsedAddresses();
-      const address = usedAddress[0];
 
-      // create forgingScript
-      const forgingScript = ForgeScript.withOneSignature(address);
+      try {
+        // Get address from the wallet
+        const usedAddress = await wallet.getUsedAddresses();
+        const address = usedAddress[0];
 
-      const assetMetadata: AssetMetadata = {
-        "name": "Mesh Token",
-        "image": "ipfs://",
-        "mediaType": "image/jpg",
-        "description": "This NFT"
-      };
-      
-      const asset: Mint = {
-        assetName: 'Careblock',
-        assetQuantity: '1',
-        metadata: assetMetadata,
-        label: '721',
-        recipient: '' 
-      };
+        // Create forgingScript
+        const forgingScript = ForgeScript.withOneSignature(address);
 
-      const tx = new Transaction({ initiator: wallet });
-      tx.mintAsset(
-        forgingScript,
-        asset,
-      );
-      
-      const unsignedTx = await tx.build();
-      const signedTx = await wallet.signTx(unsignedTx);
-      const txHash = await wallet.submitTx(signedTx);
+        const asset: Mint = {
+          assetName: 'Careblock',
+          assetQuantity: '1',
+          metadata: metadata as AssetMetadata,
+          label: '721',
+          recipient: recipient || address, // Default to own address if recipient not provided
+        };
+
+        const tx = new Transaction({ initiator: wallet });
+        tx.mintAsset(
+          forgingScript,
+          asset,
+        );
+
+        const unsignedTx = await tx.build();
+        const signedTx = await wallet.signTx(unsignedTx);
+        const txHash = await wallet.submitTx(signedTx);
+
+        console.log("Transaction hash:", txHash);
+      } catch (error) {
+        console.error("Error creating EHR:", error);
+      }
 
       setLoading(false);
     }
@@ -51,20 +53,55 @@ const Home: NextPage = () => {
     <div>
       <h1>Connect Wallet</h1>
       <CardanoWallet />
+
       {connected && (
         <>
           <h1>Create EHR</h1>
-            <button
-              type="button"
-              onClick={() => createEHR()}
-              disabled={loading}
-              style={{
-                margin: "8px",
-                backgroundColor: loading ? "orange" : "grey",
-              }}
-            >
-              Make EHR
-            </button>
+          <div>
+            <label>
+              Name: 
+              <input
+                type="text"
+                placeholder="Asset name"
+                onChange={(e) =>
+                  setMetadata((prev) => ({ ...prev, name: e.target.value }))
+                }
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Description: 
+              <input
+                type="text"
+                placeholder="Asset description"
+                onChange={(e) =>
+                  setMetadata((prev) => ({ ...prev, description: e.target.value }))
+                }
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Recipient: 
+              <input
+                type="text"
+                placeholder="Recipient address"
+                onChange={(e) => setRecipient(e.target.value)}
+              />
+            </label>
+          </div>
+          <button
+            type="button"
+            onClick={() => createEHR()}
+            disabled={loading}
+            style={{
+              margin: "8px",
+              backgroundColor: loading ? "orange" : "grey",
+            }}
+          >
+            {loading ? "Creating..." : "Make EHR"}
+          </button>
         </>
       )}
     </div>
